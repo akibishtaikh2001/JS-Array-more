6ef588f27a7c07043016788933ec71726319f89d
akibishtaikh2001 <akibishtiakh2220@gmail.com>
2025-08-04 16:26:32 +0600

    complete array-looptask

---
+++ array-looptask4.js
@@ -0,0 +1,7 @@
+const statement = 'I am a hard working person'
+ const word = statement.split(' ');
+ const reverseWord = word.reverse();
+ const join = reverseWord.join(' ');
+ console.log(join);
+ 
+ 
