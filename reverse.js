89b42d53ad82dbc9f1b3a41e6b10fc937b4e0413
akibishtaikh2001 <akibishtiakh2220@gmail.com>
2025-08-04 13:31:18 +0600

    Revesing an Array with/Without Revese Method

---
+++ reverse.js
@@ -0,0 +1,38 @@
+//              [7, 6, 5, 4, 3, 2, 1]
+const numbers = [1, 2, 3, 4, 5, 6, 7];
+
+// console.log(numbers);
+// const reversed = numbers.reverse();
+// console.log(reversed);
+
+const revNumbers = [];
+
+for (const num of numbers) {
+    // console.log(num);
+    revNumbers.unshift(num);
+}
+//  console.log(revNumbers);
+const reveresed_numbers = [];
+for (let i = 0; i < numbers.length; i++) {
+
+    const num = numbers[i];
+    reveresed_numbers.unshift(num);
+}
+
+// reverse side
+const  rev_rev_numbers = [];
+for (let i = numbers.length -1; i >= 0; i --){
+    const num = numbers[i];
+    // console.log(num);
+    rev_rev_numbers.push(num);
+    
+}
+
+// console.log(rev_rev_numbers);
+
+
+const friends = ['Elon', 'Bill', 'Mark', 'waren'];
+
+const reverseFriends = friends.reverse();
+
+console.log( reverseFriends);
